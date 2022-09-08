deb1afe237ef1796b2731d074f06c31daf7167f4
hieudnguyen <hieudnguyen@meu-solutions.com>
2022-09-08 23:32:19 +0700

    booking

---
+++ HM.BLL/BookingSvc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HM.Common.BLL;
+using HM.DAL;
+using HM.DAL.Models;
+using HM.Common.Rsp;
+using HM.Common.Req;
+
+namespace HM.BLL
+{
+    public class BookingSvc : GenericSvc<BookingRep, Booking>
+    {
+        public override SingleRsp Read(int id)
+        {
+            var res = new SingleRsp();
+            var m = _rep.Read(id);
+            if (m == null)
+                res.SetError("404", "not found");
+            else
+            {
+                BookingReq result = new BookingReq
+                {
+                    Id = m.Id,
+                    StartDate = m.StartDate,
+                    EndDate = m.EndDate,
+                    CheckIn = m.CheckIn,
+                    RoomId = m.RoomId,
+                    UserId = m.UserId,
+                };
+                res.SetData("200", result);
+            }
+            return res;
+        }
+
+        public SingleRsp CreateBooking(BookingReq bookingReq)
+        {
+            Booking? booking = new()
+            {
+                StartDate = bookingReq.StartDate,
+                EndDate = bookingReq.EndDate,
+                CheckIn = bookingReq.CheckIn,
+                RoomId = bookingReq.RoomId,
+                UserId = bookingReq.UserId
+            };
+            var res = Create(booking);
+            return res;
+        }
+        public override SingleRsp Delete(int id)
+        {
+            Booking? m = _rep.Delete(id);
+            var res = new SingleRsp();
+            if (m == null)
+                res.SetError("NotFound");
+            else
+                res.SetData("204", m);
+            return res;
+        }
+        public SingleRsp Replace(BookingReq bookingReq)
+        {
+            SingleRsp res = new();
+            if (bookingReq.Id == null)
+            {
+                res.SetError("Id can not be null");
+                return res;
+            }
+
+            var m = _rep.Read(bookingReq.Id.GetValueOrDefault());
+
+
+            if (m != null)
+            {
+                m.StartDate = bookingReq.StartDate;
+                m.EndDate = bookingReq.EndDate;
+                m.CheckIn = bookingReq.CheckIn;
+                res = Update(m);
+            }
+            return res;
+        }
+    }
+}
