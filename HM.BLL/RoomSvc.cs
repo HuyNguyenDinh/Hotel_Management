cb93e5b1b832ec9edca46470d70602b98b79d730
DinhHuy <huyn27316@gmail.com>
2022-09-08 20:22:12 +0700

    change room to room req in get all

--- HM.BLL/RoomSvc.cs
+++ HM.BLL/RoomSvc.cs
@@ -55,5 +55,24 @@ namespace HM.BLL
             };
             return base.Create(m);
         }
+        public override MultipleRsp GetAll()
+        {
+            var m = All.Select(x => new RoomReq
+            {
+                Id = x.Id,
+                Bed = x.Bed,
+                Price = x.Price,
+                IsFree = x.IsFree,
+                Picture = x.Picture,
+                MaxHumans = x.MaxHumans,
+                RoomTypeId = x.RoomTypeId
+            }).ToList();
+            var res = new MultipleRsp();
+            if (m != null)
+                res.SetData(new List<object>(m), "200");
+            else
+                res.SetError("No room");
+            return res;
+        }
     }
 }
